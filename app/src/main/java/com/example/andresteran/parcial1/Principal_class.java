12bdf73233c80e915e33b392597e31658ab08304
Andres Teran <aftosb95>
2017-03-15 13:15:47 -0500

    PARCIAL CORTE 1

---
+++ app/src/main/java/com/example/andresteran/parcial1/Principal_class.java
@@ -0,0 +1,13 @@
+package com.example.andresteran.parcial1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal_class extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_class);
+    }
+}
