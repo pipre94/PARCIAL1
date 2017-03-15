aa2f56affd4611bcb1fab2ba3b88f278210e41ec
Andres Teran <aftosb95>
2017-03-15 13:41:53 -0500

    avance 4

--- app/src/main/java/com/example/andresteran/parcial1/Principal_class.java
+++ app/src/main/java/com/example/andresteran/parcial1/Principal_class.java
@@ -2,12 +2,21 @@ package com.example.andresteran.parcial1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Principal_class extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_principal_class);
+        showToolbar(getResources().getString(R.string.str_title_toolbar),true);
+    }
+
+    public void showToolbar(String title, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+
     }
 }
